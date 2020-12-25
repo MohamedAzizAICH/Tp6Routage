fa57fd28d0bac29ef0a58397b1c9e819a6464c36
Mohamed Aziz AICH <aichaziz159@gmail.com>
2020-12-25 22:43:14 +0100

    Routage terminé de l'application

--- src/app/app-routing.modules.ts
+++ src/app/app-routing.modules.ts
@@ -5,13 +5,16 @@ import { AccueilComponent } from './composants/accueil/accueil.component';
 import { ListmaterielComponent } from './composants/informatique/listmateriel/listmateriel.component';
 import { ContactComponent } from './composants/contact/contact.component';
 import { Error404Component } from './composants/error404/error404.component';
+import { DetailmaterielComponent } from './composants/informatique/detailmateriel/detailmateriel.component';
 
 
 const appRoutes: Routes =[
     {path:'accueil', component:AccueilComponent},
     {path:'listmateriel', component:ListmaterielComponent},
+    {path:'detailmateriel/:id', component:DetailmaterielComponent},
     {path:'contact', component:ContactComponent},
     {path:'**', component:Error404Component},
+    
     {path:'', redirectTo:'accueil', pathMatch:'full'}    
 ];
 @NgModule({
--- src/app/composants/informatique/detailmateriel/detailmateriel.component.ts
+++ src/app/composants/informatique/detailmateriel/detailmateriel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Materiel } from 'src/app/models/materiel';
 import { MaterielService } from 'src/app/services/materiel.service';
 
@@ -8,17 +9,29 @@ import { MaterielService } from 'src/app/services/materiel.service';
   styleUrls: ['./detailmateriel.component.css']
 })
 export class DetailmaterielComponent implements OnInit {
-
+  
+  identifiant:number;
   afficher:boolean =false;
   materiel:Materiel;
-/*
+  router: any;
+
 onClick(){
   this.afficher = !this.afficher;
-}*/
-  constructor(private materielService:MaterielService) { }
+}
+  constructor(private materielService:MaterielService,
+              private activatedRoute:ActivatedRoute
+
+              // private router:Router // 
+              
+              ) { }
 
   ngOnInit(): void {
-    this.materiel= this.materielService.getMaterielById("2");
+    this.identifiant = this.activatedRoute.snapshot.params["id"];
+    this.materiel= this.materielService.getMaterielById("2");  //this.identifiant// <== parametre     ;
   }
-
+  
+  onBackButtonClick(): void{
+    this.router.navigate(['/listmateriel']);
+  }
+ 
 }
